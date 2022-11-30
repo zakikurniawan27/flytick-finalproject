1db1fcc56d353ebf2e25c829f04386a2d2e18736
Muhammad Zaki kurniawan <kurniawan.zaki279@gmail.com>
2022-11-30 22:48:14 +0700

    feature: add homePage

---
+++ src/components/card/CardsExplore.js
@@ -0,0 +1,56 @@
+import React from "react";
+import imageCardExplore from "../../assets/card-explore.png";
+
+function CardsExplore() {
+  return (
+    <>
+      <div className="card card-explore" role="button">
+        <img
+          src={imageCardExplore}
+          className="card-img-top img-explore"
+          alt="imagecard1"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Stay among the atolls in <span className="text-success-900">Maldives</span></h5>
+          <p className="card-text">
+            From the 2nd century AD, the islands were known as the
+            'Money Isles' due to the abundance of cowry shells, a currency of
+            the early ages..
+          </p>
+        </div>
+      </div>
+      <div className="card card-explore" role="button">
+        <img
+          src={imageCardExplore}
+          className="card-img-top img-explore"
+          alt="imagecard1"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Stay among the atolls in <span className="text-success-900">Maldives</span></h5>
+          <p className="card-text">
+            From the 2nd century AD, the islands were known as the
+            'Money Isles' due to the abundance of cowry shells, a currency of
+            the early ages..
+          </p>
+        </div>
+      </div>
+      <div className="card card-explore" role="button">
+        <img
+          src={imageCardExplore}
+          className="card-img-top img-explore"
+          alt="imagecard1"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Stay among the atolls in <span className="text-success-900">Maldives</span></h5>
+          <p className="card-text">
+            From the 2nd century AD, the islands were known as the
+            'Money Isles' due to the abundance of cowry shells, a currency of
+            the early ages..
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CardsExplore;
