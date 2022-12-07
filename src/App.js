d45b88104f45bff8f111bfa8907cf803f5b3c10b
ainnadias <ainnadias@students.amikom.ac.id>
2022-12-07 22:10:53 +0700

    Add login and register page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import SignIn from "./pages/SignIn/SignIn";
-import SignUp from "./pages/SignUp/SignUp";
-import User from "./pages/Account/User";
 
 function App() {
   return (
@@ -14,9 +11,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/user" element={<User />} />
       </Routes>
       <Footer />
     </>
