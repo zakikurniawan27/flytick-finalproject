893fcf4f2c9e07eb2a79a3ba981d5df2e16422d4
ainnadias <ainnadias@students.amikom.ac.id>
2022-12-20 05:45:09 +0700

    integrate profile page tapi bagian image masih eror

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,26 @@
-import React, { useEffect } from "react"
-import logo from "../assets/logo.png"
-import { useNavigate } from "react-router-dom"
-import { BsPersonCircle, BsBell } from "react-icons/bs"
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import logo from "../assets/logo.png";
+import { useNavigate } from "react-router-dom";
+import { BsPersonCircle, BsBell } from "react-icons/bs";
+import axios from "axios";
 
-function Navbar({token, setToken}) {
+function Navbar({ token, setToken }) {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
-
-  useEffect(() =>{
+  const [user, setUser] = useState([]);
+  // console.log(user);
+  useEffect(() => {
     (async () => {
       if (token) {
         try {
-          await axios.get(`${process.env.REACT_APP_BASE_URL}/api/auth/whoami`, {
+          const data = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/auth/whoami`, {
             headers: {
               Authorization: `${token}`,
             },
           });
+
+          setUser(data.data.data);
+          // console.log(data.data.data);
         } catch (error) {
           if (error.response.status === 401) {
             localStorage.removeItem("token");
@@ -26,30 +30,22 @@ function Navbar({token, setToken}) {
         }
       }
     })();
-  }, [token, navigate, setToken])
+  }, [token, navigate, setToken]);
 
-  const handleLogout = (e) =>{
-    e.preventDefault()
-    localStorage.removeItem("token")
-    setToken(null)
-  }
+  const handleLogout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    setToken(null);
+  };
 
   return (
     <>
       <nav className="navbar navbar-expand-md">
         <div className="container-fluid">
           <a className="navbar-brand ms-2" href="/">
-            <img src={logo} alt="logo" width="120" height="75" id="logo"/>
+            <img src={logo} alt="logo" width="120" height="75" id="logo" />
           </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarScroll"
-            aria-controls="navbarScroll"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse " id="navbarScroll">
@@ -67,21 +63,31 @@ function Navbar({token, setToken}) {
                     </a>
                   </li>
                   <li className="nav-item">
-                    <button className="btn bttn mt-1" onClick={() => navigate("/signup")}>Sign Up</button>
+                    <button className="btn bttn mt-1" onClick={() => navigate("/signup")}>
+                      Sign Up
+                    </button>
                   </li>
                 </>
-              ):(
+              ) : (
                 <>
                   <div className="btn-group me-auto">
                     <button className="btn" type="button">
-                      <BsBell className="fs-6"/>
+                      <BsBell className="fs-6" />
                     </button>
                     <button className="btn " type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      <BsPersonCircle className="fs-3"/> 
+                      <BsPersonCircle className="fs-3" />
                     </button>
                     <ul className="dropdown-menu dropdown-menu-lg-end">
-                      <li><a className="dropdown-item" href="/user">My Profil</a></li>
-                      <li><button className="dropdown-item" onClick={handleLogout}>Logout</button></li>
+                      <li>
+                        <a className="dropdown-item" onClick={() => navigate(`/user/${user.id}`)}>
+                          My Profil
+                        </a>
+                      </li>
+                      <li>
+                        <button className="dropdown-item" onClick={handleLogout}>
+                          Logout
+                        </button>
+                      </li>
                     </ul>
                   </div>
                 </>
